068ab5337251bdb6fe7b86bfffb2cc3409040216
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-02 12:27:26 -0500

    added a database and Article model

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blog.models import Article
 import datetime
 
 
 def home_page(request):
     now = str(datetime.datetime.now())
-    context = {'name': 'Jeff', 'day': now}
+    articles = Article.objects.all()
+    context = {'name': 'Jeff', 'day': now, 'articles': articles}
     response = render(request, 'index.html', context)
     return HttpResponse(response)
